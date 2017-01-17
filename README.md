afa9a9c9b38547606193d50fec1dbd07fd089413
Michael Shaw <michaeljshaw@gmail.com>
2017-01-18 00:35:25 +1100

    Basic ogg playing

--- README.md
+++ README.md
@@ -10,9 +10,10 @@ cargo run --example commands
 # Todo
 - Make launch application return a result and try! (we'd have to translate errors)
 - Fix Texture/render directions
+- We should probably have a locals struct, simple so we can re-use the struct between pipelines (blend/non-blending versions)
 - Alpha blending (look at gfx-rs particle exapmle, requires new pipeline)
 - Work out better placeholder geometry (so we can cut the Tesselator creation in fatter.rs)
 - Alto (openal) + Lewton (ogg) for sound?
 - Put color on a diet
 - Add text rendering from ggez.rs
-- We should probably have a locals struct, simple so we can re-use the struct between pipelines (blend/non-blending versions)
+
---
+++ examples/sound/main.rs
@@ -0,0 +1,129 @@
+#![allow(dead_code)]
+
+extern crate jam;
+
+use alto::*;
+use std::sync::Arc;
+
+extern crate alto;
+extern crate ogg;
+extern crate lewton;
+
+use lewton::VorbisError;
+use lewton::inside_ogg::OggStreamReader;
+
+use std::path::Path;
+use std::fs::File;
+use std::iter::Extend;
+
+pub type SampleRate = u32;
+
+#[derive(Clone, Debug)]
+struct Sound {
+    data : Vec<i16>,
+    sample_rate: u32,
+    channels: u8,
+}
+
+fn load_ogg<P: AsRef<Path>>(path: P) -> Result<Sound, VorbisError> {
+    let f = try!(File::open(path));
+
+	// Prepare the reading
+    let packet_reader = ogg::PacketReader::new(f);
+	let mut srr = try!(OggStreamReader::new(packet_reader));
+    
+    if srr.ident_hdr.audio_channels > 2 {
+		// the openal crate can't process these many channels directly
+        // std::vec::Vec<i16>
+		println!("Stream error: {} channels are too many!", srr.ident_hdr.audio_channels);
+	}
+
+    // let mut len_play = 0.0;
+    let mut data : Vec<i16> = Vec::new();
+    while let Some(pck_samples) = try!(srr.read_dec_packet_itl()) {
+        // println!("I got some shit {:?}", pck_samples);
+        // len_play += pck_samples.len() as f32 / srr.ident_hdr.audio_sample_rate as f32;
+        data.extend(pck_samples.iter());
+    }
+    
+    Ok(Sound {
+        data: data,
+        sample_rate: srr.ident_hdr.audio_sample_rate,
+        channels: srr.ident_hdr.audio_channels,
+    })
+}
+
+fn main() {
+//    let alto = Alto::load_default().unwrap();
+    let alto = Alto::load("./OpenAL64.dll").unwrap();
+ 	println!("Using output: {:?}", alto.default_output().unwrap());
+
+	let dev = alto.open(None).unwrap();
+	let ctx = dev.new_context(None).unwrap();
+
+    let sound = load_ogg("../coins.ogg").unwrap();
+    // println!("sound -> {:?}", sound);
+    println!("gonna get buff");
+    let mut buf = ctx.new_buffer().unwrap();
+    println!("got buff");
+    buf.set_data::<Mono<i16>, _>(sound.data, sound.sample_rate as i32).unwrap();
+    let buf = Arc::new(buf);
+
+    let mut src = ctx.new_static_source().unwrap();
+    src.set_buffer(buf).unwrap();
+    src.set_looping(true).unwrap();
+
+    println!("Playing static 440hz sine wave...");
+    src.play().unwrap();
+
+    std::thread::sleep(std::time::Duration::new(2, 0));
+    // {
+	// 	let mut buf = ctx.new_buffer().unwrap();
+	// 	// buf.set_data(SinWave::new(44_000 / 440, 0.25).render().take(44_000 / 440).collect::<Vec<_>>(), 44_000).unwrap();
+	// 	let buf = Arc::new(buf);
+
+	// 	let mut src = ctx.new_static_source().unwrap();
+	// 	src.set_buffer(buf).unwrap();
+	// 	src.set_looping(true).unwrap();
+
+	// 	println!("Playing static 440hz sine wave...");
+	// 	src.play().unwrap();
+
+	// 	std::thread::sleep(std::time::Duration::new(2, 0));
+	// }
+}
+
+
+
+struct SinWave {
+	len: i32,
+	vol: f32,
+	cursor: i32,
+}
+
+struct SinWaveRenderer<'w>(&'w mut SinWave);
+
+
+impl SinWave {
+	pub fn new(len: i32, vol: f32) -> SinWave {
+		SinWave{len: len, vol: vol, cursor: 0}
+	}
+
+
+	pub fn render(&mut self) -> SinWaveRenderer {
+		SinWaveRenderer(self)
+	}
+}
+
+
+impl<'w> Iterator for SinWaveRenderer<'w> {
+	type Item = Mono<i16>;
+
+	fn next(&mut self) -> Option<Mono<i16>> {
+		let cursor = self.0.cursor;
+		self.0.cursor += 1;
+		if self.0.cursor == self.0.len { self.0.cursor = 0 }
+
+		Some(Mono{center: ((cursor as f32 / self.0.len as f32 * 2.0 * std::f32::consts::PI).sin() * self.0.vol * std::i16::MAX as f32) as i16})
+	}
+}
